6d582f43a5ede76ec18ea43dac86fadd6cea3d2a
cyplenokk <ivanova.polina.ig@gmail.com>
2024-12-26 02:07:13 +0300

    Push 4

---
+++ app/src/main/java/com/example/bookshelf/ui/BooksApp.kt
@@ -0,0 +1,58 @@
+package com.example.bookshelf.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.bookshelf.R
+import com.example.bookshelf.data.Book
+import com.example.bookshelf.ui.screens.HomeScreen
+
+@Composable
+fun BooksApp(
+    modifier: Modifier = Modifier,
+    onBookClicked: (Book) -> Unit
+) {
+    val booksViewModel: BooksViewModel =
+        viewModel(factory = BooksViewModel.Factory)
+    val searchWidgetState = booksViewModel.searchWidgetState
+    val searchTextState = booksViewModel.searchTextState
+
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            MainAppBar(
+                searchWidgetState = searchWidgetState.value,
+                searchTextState = searchTextState.value,
+                onTextChange = {
+                               booksViewModel.updateSearchTextState(newValue = it)
+                },
+                onCloseClicked = {
+                                 booksViewModel.updateSearchWidgetState(newValue = SearchWidgetState.CLOSED)
+                },
+                onSearchClicked = {
+                    booksViewModel.getBooks(it)
+                },
+                onSearchTriggered = {
+                    booksViewModel.updateSearchWidgetState(newValue = SearchWidgetState.OPENED)
+                }
+            )
+        }
+    ) {
+        Surface(modifier = modifier
+            .fillMaxSize()
+            .padding(it),
+            color = MaterialTheme.colors.background
+        ) {
+            HomeScreen(
+                booksUiState = booksViewModel.booksUiState,
+                retryAction = { booksViewModel.getBooks() },
+                modifier = modifier,
+                onBookClicked
+            )
+        }
+    }
+}
